0d52bb1998ae8f01613fe6c56cbdc02f4650099f
Trainwagon <pangeranmlg278@gmail.com>
2025-01-16 19:45:45 +0700

    improve ui

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -90,21 +90,6 @@ const Add = () => {
               required
             />
           </div>
-          <div>
-            <label
-              htmlFor="cover"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Cover URL
-            </label>
-            <input
-              type="text"
-              id="cover"
-              name="cover"
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300"
--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -107,22 +107,7 @@ const Update = () => {
               required
             />
           </div>
-          <div>
-            <label
-              htmlFor="cover"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Cover URL
-            </label>
-            <input
-              type="text"
-              id="cover"
-              name="cover"
-              value={book.cover}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
+
           <button
             type="submit"
             className="w-full bg-yellow-500 text-white py-2 px-4 rounded-lg hover:bg-yellow-600 transition duration-300"
